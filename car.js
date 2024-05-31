009ada3323fe933840863b76779051aa1b118210
shokhrukh-rt <mr.shohruh.1003@gmail.com>
2024-05-31 16:11:43 +0900

    Finished the movement method of the car

--- car.js
+++ car.js
@@ -7,13 +7,28 @@ class Car {
 
 		this.speed = 0;
 		this.acceleration = 0.2;
-		this.maxSpeed = 3;
+		this.maxSpeed = 1.8;
 		this.friction = 0.05;
+		this.angle = 0;
 
 		this.controls = new Controls();
 	}
 
 	update() {
+		this.#move();
+	}
+
+	draw(ctx) {
+		ctx.save();
+		ctx.translate(this.x, this.y);
+		ctx.rotate(-this.angle);
+		ctx.beginPath();
+		ctx.rect(-this.width / 2, -this.height / 2, this.width, this.height);
+		ctx.fill();
+		ctx.restore();
+	}
+
+	#move() {
 		if (this.controls.forward) {
 			this.speed += this.acceleration;
 		}
@@ -36,17 +51,21 @@ class Car {
 			this.speed += this.friction;
 		}
 
-		this.y -= this.speed;
-	}
+		if (Math.abs(this.speed) < this.friction) {
+			this.speed = 0;
+		}
 
-	draw(ctx) {
-		ctx.beginPath();
-		ctx.rect(
-			this.x - this.width / 2,
-			this.y - this.height / 2,
-			this.width,
-			this.height
-		);
-		ctx.fill();
+		if (this.speed != 0) {
+			const flip = this.speed > 0 ? 1 : -1;
+			if (this.controls.left) {
+				this.angle += 0.01 * flip;
+			}
+			if (this.controls.right) {
+				this.angle -= 0.01 * flip;
+			}
+		}
+
+		this.x -= Math.sin(this.angle) * this.speed;
+		this.y -= Math.cos(this.angle) * this.speed;
 	}
 }
